839fefbb6cb448dfe631bdc47f5fb2f8e2b479f7
Juan Carlos Guerrero Borunda <juancgb00@gmail.com>
2023-09-28 11:50:47 -0600

    Movies_providers,Api,Now_Playin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_unidad1/screens/screens.dart';
+import 'package:provider/provider.dart';
+import 'providers/movies_providers.dart';
+void main() => runApp(const AppState());
 
-void main() => runApp(const MyApp());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        )
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
